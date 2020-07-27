37c32e245d3facd085126db84ead023c71f5e2ba
candidroot <39722234+candidroot@users.noreply.github.com>
2020-07-27 17:25:59 +0530

    Update __init__.py

--- barcode_scan_weight/__init__.py
+++ barcode_scan_weight/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo Module Developed by Candidroot Solutions Pvt. Ltd.
+# Part of Odoo Module Developed by Candidroot Solutions Pvt. Ltd
 # See LICENSE file for full copyright and licensing details.
